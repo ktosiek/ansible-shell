7d6a6a3845f32e75d58613e1988923fe3d546526
Tomasz Kontusz <tomasz.kontusz@gmail.com>
2014-11-29 21:56:41 +0100

    Test executing "setup" against localhost

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 import pytest
 import imp
+import subprocess
 import os
 
 ansible_shell_path = os.path.dirname(__file__) + '/ansible-shell'
@@ -17,3 +18,15 @@ with open(ansible_shell_path) as f:
 ])
 def test_generating_batches(hosts, serial, result):
     assert result == ansible_shell.get_hosts_batches(hosts, serial)
+
+
+def test_running_ansible_shell(tmpdir):
+    inv = tmpdir.join('inventory')
+    inv.write('localhost ansible_connection=local\n')
+    process = subprocess.Popen([ansible_shell_path, '-i', str(inv)],
+                               stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+
+    stdout, _ = process.communicate('cd all\nsetup\n')
+    print stdout
+
+    assert '"ansible_hostname"' in stdout
