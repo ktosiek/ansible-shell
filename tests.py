b9d7b30f4dc6948309e78c4e5d8f984190bf64fd
Tomasz Kontusz <tomasz.kontusz@gmail.com>
2014-12-03 23:14:06 +0100

    Test running ansible-shell with vault password file

--- tests.py
+++ tests.py
@@ -30,3 +30,36 @@ def test_running_ansible_shell(tmpdir):
     print stdout
 
     assert '"ansible_hostname"' in stdout
+
+
+def test_running_with_vault(tmpdir):
+    inv = tmpdir.join('inventory')
+    inv.write('localhost ansible_connection=local\n')
+
+    password_file = tmpdir.join('passwd')
+    password_file.write('secret_password')
+
+    all_vars = tmpdir.mkdir('group_vars').join('all')
+    all_vars.write('dummy_var: secret_value')
+    subprocess.check_call([
+        'ansible-vault', 'encrypt',
+        '--vault-password-file', str(password_file),
+        str(all_vars)
+    ])
+
+    assert 'secret_value' not in all_vars.read()
+
+    process = subprocess.Popen([
+        ansible_shell_path, '-i', str(inv),
+        '--vault-password-file', str(password_file)],
+        stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+
+    stdout, _ = process.communicate('\n'.join([
+        'secret_password',
+        'cd all',
+        '!echo {{ dummy_var }}',
+        '\n'
+    ]))
+    print stdout
+
+    assert 'secret_value' in stdout
